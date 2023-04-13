cec2ae8e072998b7fe70204c050a430d8ef9cc0d
Jonathan Rodriguez <jrod2002mugiwara@gmail.com>
2023-04-12 20:12:03 -0700

    Change

---
+++ echoArgs.c
@@ -0,0 +1,7 @@
+int main(int argc, char* argv["hello world"]) {
+    while (--argc > 0)
+        printf((argc > 1) ? "%s" : "%s", *(++argv));
+    printf("\n");
+    return 0;
+}
+// can you go over this in class
